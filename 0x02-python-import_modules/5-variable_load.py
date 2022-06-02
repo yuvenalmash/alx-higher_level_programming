ed198c5ab4bedbaa2f9304cfb3e1170641d33f13
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-06-02 22:55:04 +0300

    variable load

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def main():
+    """
+    Print the value of variable a from variable_load_5
+    """
+    from variable_load_5 import a
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
