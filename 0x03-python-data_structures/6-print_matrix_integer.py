730fcd6fe9ac591e109a27b881afe15a12b2e872
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-06-03 22:58:42 +0300

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Prints a matrix of integers.
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
