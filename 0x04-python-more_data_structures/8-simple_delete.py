53c8d404ab6042cf1b197de27a31af112949ff8c
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-06-08 14:47:56 +0300

    simple delete

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """
+    Deletes a key in a dictionary.
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
