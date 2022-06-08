bd508f6c4bb62a4b19046833580ab90878881370
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-06-08 14:22:55 +0300

    search_replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element by another in a new list
+    """
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
