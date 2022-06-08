9633471110683826067de3ed56a0d631b0fb1728
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-06-08 14:44:52 +0300

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value in a dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
