1c69b0a5b79730ea3c7ac017c2451f4965d047fd
yuvenal <yuvenalmash@gmail.com>
2022-07-05 22:58:11 +0300

    search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Search and update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts text after each line containing 
+    a given string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search 
+        for within the file.
+        new_string (str): The string to insert.
+
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
