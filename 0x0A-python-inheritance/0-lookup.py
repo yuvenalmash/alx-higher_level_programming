04fb91e0f287d3b7f94ee57cf17af76491329a1a
yuvenal <yuvenalmash@gmail.com>
2022-07-06 23:02:27 +0300

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""lookup"""
+
+
+def lookup(obj):
+    """A function that returns the list of available
+    attributes and methods of an object
+    Returns a list object
+
+    """
+    return dir(obj)
